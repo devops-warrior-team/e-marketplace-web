db4db5b746c889b41504ac1824b74a3d5a89743a
feriva22 <fericodeva69@gmail.com>
2023-08-01 15:07:38 +0000

    update docker env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -R /var/www/tmp/. /var/www/html/
+#cp -R /var/www/html/.env.example /var/www/html/.env
 chown -R www-data:www-data /var/www/html
 
 #instructs the shell to run whatever command was passed in as an input argument next.
